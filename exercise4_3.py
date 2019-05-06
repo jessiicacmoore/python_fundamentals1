630dbc3ba21d30e3ad4ada67185409c3bbb9a9d7
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-06 15:00:06 -0400

    Completed exercise 4 part 3

---
+++ exercise4_3.py
@@ -0,0 +1,12 @@
+my_name = "Jessica"
+
+print("Hello, what's your name?")
+user_name = input()
+
+if user_name.lower() == my_name.lower() :
+  print("Hey, we have the same name!")
+
+else:
+  print("We don't have the same name, sorry.")
+
+
