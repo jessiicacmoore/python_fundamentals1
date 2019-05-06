d74fda57e9222cc7bc7f0e47305c07eb3dee17b3
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-06 14:37:06 -0400

    Completed exercise 4 part 1

---
+++ exercise4_1.py
@@ -0,0 +1,13 @@
+# Retrieve number from user
+print("Pick a number, any number!")
+user_number = input()
+
+if int(user_number) >= 100:
+  print("That's a big number!")
+
+# if int(user_number) < 100:
+#   print("Why not dream a little bigger?")
+
+# using else
+else:
+  print("Why not dream a little bigger?")
